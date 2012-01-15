ee9a085ba618485684fbf94bf618466b0edc0302
willemw12 <willemw12@gmail.com>
2012-01-15 19:26:59 +0100

    x-terminal-emulator

--- src/get_iplayer_downloader/tools/command.py
+++ src/get_iplayer_downloader/tools/command.py
@@ -7,21 +7,23 @@ logger = logging.getLogger(__name__)
 #def run(cmd, run_in_terminal_window=False, terminal_title=None, quiet=False, **keywords):
 def run(cmd, run_in_terminal_window=False, terminal_title=None, quiet=False,
         log_level=logging.DEBUG, temp_pathname=None):
-    if not quiet:
-        logger.log(log_level, "run(cmd): cmd=%s", cmd)
-        if log_level == logging.DEBUG:
+    if not quiet and log_level == logging.DEBUG:
             #temp_pathname = keywords["temp_pathname"] if "temp_pathname" in keywords else None
             if temp_pathname is not None:
                 # Add commands to log to file
                 if not os.path.exists(temp_pathname):
                     os.makedirs(temp_pathname)
                 #cmd += " 2>&1 | tee $(mktemp " + temp_pathname + "/cmd-XXXXXXXXXX.log)"
-                cmd += " 2>&1 | tee " + temp_pathname + "/$(date +\"%Y%m%d%H%M%S\")-cmd.log"
+                cmd = "(" + cmd + ") 2>&1 | tee " + temp_pathname + "/$(date +\"%Y%m%d%H%M%S\")-cmd.log"
 
     if run_in_terminal_window:
         # Add commands to run in a terminal window
         if os.name == "posix":
-            cmd = "x-terminal-emulator --geometry=132x43 --title=\"" + str(terminal_title) + "\" --command=\"bash -c '" + cmd + " ; cd ; $SHELL'\" ; exit 0"
+            #cmd = "gnome-terminal --geometry=131x41 --title=\"" + str(terminal_title) + "\" --command=\"sh -c '" + cmd + " ; cd ; sh'\" ; exit 0"
+            cmd = "x-terminal-emulator -geometry 131x41 -title \"" + str(terminal_title) + "\" -e \"$SHELL -c '" + cmd + " ; cd ; $SHELL'\" ; exit 0"
+
+    if not quiet:
+        logger.log(log_level, "run(cmd): cmd=%s", cmd)
 
     process_output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
 
