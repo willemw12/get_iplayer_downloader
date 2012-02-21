ad05c128905b8e646f6df4f9a77f0a0bb7a48ac4
willemw12 <willemw12@gmail.com>
2012-02-21 09:07:14 +0100

    install scripts

--- uninstall.sh
+++ uninstall.sh
@@ -2,11 +2,14 @@
 
 echo "Uninstalling..."
 
-echo "Removing the following files (not the containing directories)..."
+echo "Removing the following files (and not the containing directories)..."
 FILE_LIST="installed-files.txt"
 cat "$FILE_LIST"
 if [ -f "$FILE_LIST" ]; then
   cat "$FILE_LIST" | xargs sudo rm -rfv
+  rm -f "$FILE_LIST"
+else
+  echo "$(basename $0): Cannot uninstall get_iplayer_downloader" >&2
 fi
 
 echo "Removing desktop menu item..."
