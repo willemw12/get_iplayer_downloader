4c2ccbc195909a42266f0875d6cef06d0a15d78b
willemw12 <willemw12@gmail.com>
2012-03-06 20:41:18 +0100

    rename config file

--- src/get_iplayer_downloader/settings.py
+++ src/get_iplayer_downloader/settings.py
@@ -10,12 +10,12 @@ from get_iplayer_downloader.tools import config as Config
 
 # The default config file serves as a, per property, factory setting lookup fallback
 #NOTE __file__ is not defined when run from the interpreter
-DEFAULT_CONFIG_FILENAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), "default.config")
+DEFAULT_CONFIG_FILENAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), "default.conf")
 
 if os.name == "posix":
-    USER_CONFIG_FILENAME = os.path.join(os.path.expanduser("~"), ".config", common.__program_name__, "config")
+    USER_CONFIG_FILENAME = os.path.join(os.path.expanduser("~"), ".config", common.__program_name__, common.__program_name__ + "config")
 else:
-    USER_CONFIG_FILENAME = os.path.join(os.path.expanduser("~"), common.__program_name__, "config")
+    USER_CONFIG_FILENAME = os.path.join(os.path.expanduser("~"), common.__program_name__, common.__program_name__ + "config")
     
 TEMP_PATHNAME = tempfile.gettempdir() + os.sep + common.__program_name__
 
