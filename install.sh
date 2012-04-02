f048d4bdb13f2af2b700a7bd937489a7cb39b968
willemw12 <willemw12@gmail.com>
2012-04-02 10:59:58 +0200

    install scripts

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp /tmp/installed-files.txt .
 sudo rm /tmp/installed-files.txt
 
 sudo python setup.py clean 
-sudo rm -rf build
+sudo rm -r build
 
 echo "Installing desktop menu item..."
 sudo xdg-desktop-menu install --novendor res/get_iplayer_downloader.desktop
--- uninstall.sh
+++ uninstall.sh
@@ -6,8 +6,11 @@ echo "Removing the following files (and not the containing directories)..."
 FILE_LIST="installed-files.txt"
 cat "$FILE_LIST"
 if [ -f "$FILE_LIST" ]; then
-  cat "$FILE_LIST" | xargs sudo rm -rfv
-  rm -f "$FILE_LIST"
+  # Force sudo password prompt
+  sudo -K
+
+  cat "$FILE_LIST" | xargs sudo rm -rf
+  rm "$FILE_LIST"
 else
   echo "$(basename $0): Cannot uninstall get_iplayer_downloader" >&2
 fi
