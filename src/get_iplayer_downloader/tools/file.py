16018c194d71540540056e9a700933b5c930a56b
willemw12 <willemw12@gmail.com>
2014-04-15 15:21:46 +0200

    update subdir substitution

--- src/get_iplayer_downloader/tools/file.py
+++ src/get_iplayer_downloader/tools/file.py
@@ -64,29 +64,53 @@ def files2urls(filepath):
 
 def sanitize_path(path, include_substitution_markers):
     """ Sanitize file name path @path, similar to the get_iplayer rules.
-        @include_substitution_markers: include substitution marker characters < and > in the sanitation process    
+        @include_substitution_markers: include substitution marker characters < and > in the sanitization process    
     """
-    
+
+    # Partly based on 'sub StringUtils::sanitize_path' in get_iplayer
+    r"""
+    sub StringUtils::sanitize_path {
+        my $string = shift;
+        my $is_path = shift || 0;
+        # (1) Replace leading/trailing ellipsis with _
+        $string =~ s/(^\.+|\.+$)/_/g;
+        # (2) Replace forward slashes with _ if not path
+        $string =~ s/\//_/g unless $is_path;
+        # (3) Replace backslashes with _ if not Windows path
+        $string =~ s/\\/_/g unless $^O eq "MSWin32" && $is_path;
+        # (4) Remove extra/leading/trailing whitespace
+        $string =~ s/\s+/ /g;
+        $string =~ s/(^\s+|\s+$)//g;
+        # (5) Replace whitespace with _ unless --whitespace
+        $string =~ s/\s+/_/g unless $opt->{whitespace};
+        # (6) Truncate multiple replacement chars
+        $string =~ s/_+/_/g;
+        # (7) Remove non-ASCII and most punctuation unless --whitespace
+        $string =~ s/[^a-zA-Z0-9_\-\.\/\s]//gi unless $opt->{whitespace};
+        # (8) Remove FAT forbidden chars if --fatfilename
+        $string =~ s/[\|\?\*\+\"\:\<\>\[\]]//g if $opt->{fatfilename};
+        # (9) Remove non-ASCII if --fatfilename
+        $string =~ s/[^\x{20}-\x{7E}]//g if $opt->{fatfilename};
+        return $string;
+    }
+    """
+ 
     # Sanitize directory path, optionally including substitution marker 
     # characters < and >, i.e. collapse adjacent invalid characters into a 
     # single _ character or remove invalid characters
 
     # Match and replace ! and perhaps other characters, which as program
     # arguments have been translated by Python or get_iplayer into \! and 
-    # this would otherwise result in an _ in the sanitized path
+    # which would otherwise result in an _ in the sanitized path
     p = re.compile(r"(\\\!+)")
     path = p.sub("", path)
 
-    #PERL based on get_iplayer's Perl code
-    #sub StringUtils::sanitize_path {
-    # Remove fwd slash if reqd
-    #$string =~ s/\//_/g if ! $allow_fwd_slash;
-    # Replace backslashes with _ regardless
-    #$string =~ s/\\/_/g;
-    # Sanitize by default
-    #$string =~ s/\s+/_/g if (! $opt->{whitespace}) && (! $allow_fwd_slash);
+    # PERL comment: NOT (2) Replace forward slashes with _ if not path
+    # PERL comment: (3) Replace backslashes with _ if not Windows path
+    # PERL comment: (4) Remove extra/leading/trailing whitespace
+    # PERL comment: (5) Replace whitespace with _ unless --whitespace
     #
-    # Similar to Perl code, however, exclude matching forward slash.
+    # Similar to Perl code, however, exclude matching forward slash
     # \s means whitespace
     if os.name == "posix":
         p = re.compile(r"([\\\s]+)")
@@ -94,20 +118,20 @@ def sanitize_path(path, include_substitution_markers):
         p = re.compile(r"([\s]+)")
     path = p.sub("_", path)
 
-    #PERL based on get_iplayer's Perl code
-    #$string =~ s/[^\w_\-\.\/\s]//gi if ! $opt->{whitespace};
+    # PERL comment: NOT (1) Replace leading/trailing ellipsis with _
+    # PERL comment: (7) Remove non-ASCII and most punctuation unless --whitespace
     #
     # Similar to Perl code, however, also exclude matching substitution marker
     # characters < and >
     p = re.compile(r"([^\w_\-\.\/\s<>]+)")
     path = p.sub("", path)
 
-    #PERL based on get_iplayer's Perl code
-    #$string =~ s/[\|\\\?\*\<\"\:\>\+\[\]\/]//gi if $opt->{fatfilename};
+    # PERL comment: (8) Remove FAT forbidden chars if --fatfilename
+    # PERL comment: NOT (9) Remove non-ASCII if --fatfilename
     #
     # Similar to Perl code, however, exclude matching forward slash and 
     # substitution marker characters < and >
-    p = re.compile(r"([\|\\\?\*\"\:\+\[\]]+)")
+    p = re.compile(r"([\|\?\*\+\"\:\[\]]+)")
     path = p.sub("", path)
 
     # Replace substitution marker characters < and >
@@ -115,10 +139,9 @@ def sanitize_path(path, include_substitution_markers):
         p = re.compile(r"([<>]+)")
         path = p.sub("_", path)
 
-    #PERL based on get_iplayer's Perl code
-    # Truncate multiple '_'
-    #$string =~ s/_+/_/g;
+    # PERL comment: (6) Truncate multiple ('_') replacement chars
     p = re.compile(r"([_]+)")
     path = p.sub("_", path)
 
     return path
+
