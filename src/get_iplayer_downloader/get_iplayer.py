afc26c4ee3ef6b57adb630d084b19b55a9f3f099
willemw12 <willemw12@gmail.com>
2016-07-10 15:11:19 +0200

    Fix separating multiple get_iplayer commands

--- src/get_iplayer_downloader/get_iplayer.py
+++ src/get_iplayer_downloader/get_iplayer.py
@@ -455,7 +455,7 @@ def get(search_term_list, pid=True, pvr_queue=False, preset=None, prog_type=None
             # search_term_list could be a set of episode indices, so don't surround them with quotes
             cmd += search_term
         
-        if (i> 0 and i < len(search_term_list) - 1):
+        if (i < len(search_term_list) - 1):
             #cmd += "; "
             cmd += "; echo '----'; "
 
