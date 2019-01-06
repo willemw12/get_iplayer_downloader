90d725fafcc21dce2cc47e61176e226b1f6ce4af
willemw12 <willemw12@gmail.com>
2019-01-06 09:50:40 +0100

    Generate search cache example script: remove date

--- extra/my_generate-search-cache.sh
+++ extra/my_generate-search-cache.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-if [ $# -lt 1 ]; then
-  echo "$(basename $0): number of days missing" 1>&2
-  echo "usage: $(basename $0) <days>" 1>&2
-  exit 1
-fi
-
-DAYS="$1"
-
-#/usr/share/get_iplayer_downloader/scripts/generate_search_cache.py --type=radio,tv --categories="childrens;comedy;drama;entertainment;factual;learning" --days="$DAYS" --verbose
-/usr/share/get_iplayer_downloader/scripts/generate_search_cache.py --type=radio --categories="comedy;drama;factual;learning" --days="$DAYS" --verbose
-/usr/share/get_iplayer_downloader/scripts/generate_search_cache.py --type=tv    --categories="comedy;drama;entertainment;factual;learning" --days="$DAYS" --verbose
+#/usr/share/get_iplayer_downloader/scripts/generate_search_cache.py --type=radio,tv --categories="childrens;comedy;drama;entertainment;factual;learning" --verbose
+/usr/share/get_iplayer_downloader/scripts/generate_search_cache.py --type=radio --categories="comedy;drama;factual;learning" --verbose
+/usr/share/get_iplayer_downloader/scripts/generate_search_cache.py --type=tv    --categories="comedy;drama;entertainment;factual;learning" --verbose
 
